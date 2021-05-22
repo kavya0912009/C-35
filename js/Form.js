9957211f47416b96808de03c23fc6adbed3c1e11
Kavya09012009 <80327050+kavya0912009@users.noreply.github.com>
2021-05-22 16:59:34 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,23 @@
+class Form {
+    constructor(){
+        
+    }
+
+display(){
+ var title = createElement('h2')
+ title.html("Car Racing Game");
+ title.position(130,0);
+
+ var input = createInput("name");
+ var button = createButton('play');
+
+ input.position(130,160);
+ button.position(250,200);
+
+    
+}
+
+
+
+    
+}
